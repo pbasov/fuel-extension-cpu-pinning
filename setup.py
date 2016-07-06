75664f4111959610a4f83f586fb17e4e1c8b0ed5
Pavel Basov <pb297d@att.com>
2016-07-06 17:52:01 -0500

    Reworked extension registration for clusters and releases

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
 from setuptools import setup
+from setuptools.command.install import install
+
+from nailgun.db import db
+from nailgun.db.sqlalchemy.models import Cluster
+from nailgun.db.sqlalchemy.models import Release
+
+
+class ExtInstall(install):
+    @classmethod
+    def _set_extensions(self, nailgun_objects):
+        for obj, extensions in nailgun_objects.items():
+            extensions.append(u'cpu_pinning_override')
+            extensions = list(set(extensions))
+            obj.extensions = extensions
+            db().flush()
+
+    def run(self):
+        install.run(self)
+        clusters = {cl: cl['extensions'] for cl in db().query(Cluster).all()}
+        releases = {rl: rl['extensions'] for rl in db().query(Release).all()}
+        ExtInstall._set_extensions(clusters)
+        ExtInstall._set_extensions(releases)
+        db().commit()
 
 setup(
        name='fuel_extension_cpu_pinning',
@@ -14,6 +37,7 @@ setup(
                    'Environment :: Console',
                    ],
        packages=['fuel_extension_cpu_pinning'],
+       cmdclass={'install': ExtInstall},
        entry_points={
           'nailgun.extensions': [
              'fuel_extension_cpu_pinning = fuel_extension_cpu_pinning.extension:CpuPinningExtension',
